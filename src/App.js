37e5e00723e54ad805cfc4fb4f41e8f7a766e473
ubaidraza2006 <ubaidmemon2006@gmail.com>
2023-09-06 21:51:14 +0500

    6-9-2023

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseTracker from './Comp/Expense';
+
+function App() {
+  return (
+    <>
+    <ExpenseTracker/>
+    </>
+  );
+}
+
+export default App;
