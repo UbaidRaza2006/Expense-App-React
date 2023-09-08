36ce546b4cbbbea19c41d49a48d4d6d8a30a5970
ubaidraza2006 <ubaidmemon2006@gmail.com>
2023-09-08 17:02:45 +0500

    Edit Delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ExpenseTracker from './Comp/Expense';
+// import './global.css'
+// import { Button } from 'button';
+
 
 function App() {
   return (
     <>
     <ExpenseTracker/>
+    {/* <h1>hello</h1> */}
     </>
   );
 }
--- src/Comp/Expense.js
+++ src/Comp/Expense.js
@@ -1,16 +1,34 @@
 
-import { Button, Table, Form, Input } from 'antd';
+import { Button, Space, Table, Form, Input } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import { useEffect, useState } from 'react';
+import './global.css'
+const { Column } = Table;
 
 export default function ExpenseTracker() {
+    // naya
+    const [form] = Form.useForm();
+    // nayyaaa
+    const [transactions, setTransactions] = useState([
+        // naya 
+        {
+            created_at: new Date().toLocaleString(),
+            amount: 400,
+            type: 'income',
+            desc: "testing"
+        }
+        // nayaaa
 
-    const [transactions, setTransactions] = useState([])
+    ])
     const [type, setType] = useState(undefined)
     const [totals, setTotals] = useState({
         income: 0,
         expense: 0,
         profitLoss: 0
     })
+    // naya
+    const [isEdit, setIsEdit] = useState(null)
+    // nayaaa
 
     useEffect(() => {
         if (transactions.length) {
@@ -24,7 +42,7 @@ export default function ExpenseTracker() {
                 }
             })
 
-            setTotals({ income: income, expense:expense, profitLoss: income - expense })
+            setTotals({ income: income, expense: expense, profitLoss: income - expense })
 
         }
     }, [transactions])
@@ -34,112 +52,147 @@ export default function ExpenseTracker() {
         // console.log('dckjbcdsbc',values)
         // console.log('dckjbcdsbcjghnthyt',type)
 
-        const obj = {
-            ...values,
-            type,
-            created_at: new Date().toLocaleString()
-        }
-        console.log(obj)
-        setTransactions([obj, ...transactions])
+        // naya
+        if (isEdit !== null) {
 
+            transactions[isEdit] = {
+                ...values,
+                type,
+                created_at: new Date().toLocaleString()
+            }
+            setTransactions([...transactions])
+            setIsEdit(null)
+        } else {
+            // nayyaaaa
+            const obj = {
+                ...values,
+                type,
+                created_at: new Date().toLocaleString()
+            }
+            console.log(obj)
+            setTransactions([obj, ...transactions])
+        }
+        // naya
+        form.resetFields()
+        setType(undefined)
+        // nayaaa
     };
 
-    const columns = [
-        {
-            title: 'Date',
-            dataIndex: 'created_at',
-            key: 'created_at',
-        },
-        {
-            title: 'Amount',
-            dataIndex: 'amount',
-            key: 'amount',
-        },
-        {
-            title: 'Description',
-            dataIndex: 'desc',
-            key: 'desc',
-        },
-        {
-            title: 'Type',
-            dataIndex: 'type',
-            key: 'type',
-        },
-    ];
-    return (
-        <div className="min-h-screen p-5 bg-white flex flex-col items-center">
-            <h2 className="font-700 text-[30px]">Expense Tracker</h2>
+    // naya
+    const edit = (record, ind) => {
+        setIsEdit(ind)
+        form.setFieldsValue({
+            amount: record.amount,
+            desc: record.desc
+        })
+        setType(record.type)
+    }
+    const deleteTransaction = (ind) => {
+        transactions.splice(ind, 1)
+        setTransactions([...transactions])
+    }
+    // nayaaaa
 
-            <div className='flex'>
-                <div
-                    onClick={() => setType('income')}
-                    style={{
+    // const columns = [
+    //     {
+    //         title: 'Date',
+    //         dataIndex: 'created_at',
+    //         key: 'created_at',
+    //     },
+    //     {
+    //         title: 'Amount',
+    //         dataIndex: 'amount',
+    //         key: 'amount',
+    //     },
+    //     {
+    //         title: 'Description',
+    //         dataIndex: 'desc',
+    //         key: 'desc',
+    //     },
+    //     {
+    //         title: 'Type',
+    //         dataIndex: 'type',
+    //         key: 'type',
+    //     },
+    // ];
+    return (
+        <div id='diviii' className="min-h-screen p-5 bg-white flex flex-col items-center">
+            <h2 className="font-700 text-[30px]">Expense Tracker</h2>
+            <div className='income1'>
+
+                <div id='income' className='flex'>
+                    <div
+                        onClick={() => setType('income')}
+                        style={{
+                            borderColor: '#ccc', borderRadius: 25,
+                            cursor: 'pointer', borderWidth: 1,
+                            backgroundColor: type === 'income' ? 'green' : 'white',
+                            color: type === 'income' ? '#fff' : '#000'
+                        }} className={`px-4 py-2 m-2`}>
+                        Income
+                    </div>
+
+                    <div style={{
                         borderColor: '#ccc', borderRadius: 25,
                         cursor: 'pointer', borderWidth: 1,
-                        backgroundColor: type === 'income' ? 'green' : 'white',
-                        color: type === 'income' ? '#fff' : '#000'
-                    }} className={`px-4 py-2 m-2`}>
-                    Income
-                </div>
-
-                <div style={{
-                    borderColor: '#ccc', borderRadius: 25,
-                    cursor: 'pointer', borderWidth: 1,
-                    backgroundColor: type === 'expense' ? 'red' : 'white',
-                    color: type === 'expense' ? '#fff' : '#000'
-                }}
-                    onClick={() => setType('expense')}
-                    className={`px-4 py-2 m-2 `}>
-                    Expense
-                </div>
-            </div>
-
-            <Form
-                name="basic"
-                style={{
-                    maxWidth: 600,
-                }}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    label={'Amount'}
-                    name={'amount'}
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your amount!',
-                        },
-                    ]}
-
-                >
-                    <Input type='number' />
-                </Form.Item>
-
-                <Form.Item
-                    label={'Description'}
-                    name={'desc'}
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your description!',
-                        },
-                    ]}
-
-                >
-                    <Input type='text' />
-                </Form.Item>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
+                        backgroundColor: type === 'expense' ? 'red' : 'white',
+                        color: type === 'expense' ? '#fff' : '#000'
                     }}
+                        onClick={() => setType('expense')}
+                        className={`px-4 py-2 m-2 `}>
+                        Expense
+                    </div>
+                </div>
+
+                <Form
+                    name="basic"
+                    style={{
+                        maxWidth: 600,
+                    }}
+                    onFinish={onFinish}
                 >
-                    <Button htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                    <Form.Item
+                        label={'Amount'}
+                        name={'amount'}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your amount!',
+                            },
+                        ]}
+
+                    >
+                        <Input type='number' />
+                    </Form.Item>
+
+                    <Form.Item
+                        label={'Description'}
+                        name={'desc'}
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your description!',
+                            },
+                        ]}
+
+                    >
+                        <Input type='text' />
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button htmlType="submit">
+                       {/* naya */}
+                        {isEdit !== null ? 'Edit' : 'Submit'}
+                        {/* nayaaaa */}
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
 
             <div className='w-[700px] border-black flex'>
 
@@ -158,7 +211,26 @@ export default function ExpenseTracker() {
 
             </div>
 
-            <Table style={{ width: "700px" }} dataSource={transactions} columns={columns} />
+            <Table style={{ width: "700px" }} dataSource={transactions}
+            // columns={columns}/
+            >
+                <Column dataIndex={'created_at'} key={'created_at'} title='Date' />
+                <Column dataIndex={'amount'} key={'amount'} title='Amount' />
+                <Column dataIndex={'desc'} key={'desc'} title='Description' />
+                <Column dataIndex={'type'} key={'type'} title='Type' />
+                <Column
+                    title="Action"
+                    key="action"
+                    render={(_, record, ind) => {
+                        return (
+                            <Space size="middle">
+                                <a onClick={() => edit(record, ind)}><EditOutlined /></a>
+                                <a onClick={() => deleteTransaction(ind)}><DeleteOutlined /></a>
+                            </Space>
+                        )
+                    }} />
+            </Table>
+
 
         </div>
     )
